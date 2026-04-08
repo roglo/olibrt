461fc65afd781ee8e385fcd09e6be0fabb0ffc26
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2026-04-08 17:29:19 +0200

    -

--- Xft/XftFont.c
+++ Xft/XftFont.c
@@ -11,3 +11,16 @@ value *v;
 	);
 	return Val_addr(r);
 }
+
+value ML_XftDrawString8(v)
+value *v;
+{
+	XftDrawString8((XftDraw*) aarv(0),
+		       (_Xconst XftColor*) aarv(1),
+		       (XftFont*) aarv(2),
+		       (int) iarv(3),
+		       (int) iarv(4),
+		       (_Xconst FcChar8*) sarv(5),
+		       (int) iarv(6));
+	return unit;
+}
